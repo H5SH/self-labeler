08c5fdb36bdd451de00841ec74107a186a938cc1
Hasham Asad <tipuasad2001@gmail.com>
2024-09-06 04:37:39 +0500

    starting

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+
+const Navbar = ()=>(
+    <div className="container mx-auto flex flex-wrap p-5 flex-col items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4">
+            <img src="" />
+        </a>
+        <div className="flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5">Hello</a>
+        </div>
+    </div>
+)
+
+export default Navbar
